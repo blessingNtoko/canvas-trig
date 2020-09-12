fd52c748f646110c5b482a14d0a592810c3eacaf
Blessing <bntoko@gmail.com>
2020-09-12 13:25:51 +0200

    define functions

--- index.js
+++ index.js
@@ -13,3 +13,55 @@ let context;
 let mousePos = new MousePosition(0, 0);
 document.addEventListener("DomContentLoaded", setupCanvas);
 
+function setupCanvas() {
+    
+}
+
+function drawCanvas() {
+    
+}
+
+function reDrawCanvas() {
+    
+}
+
+function drawRectangle() {
+    
+}
+
+function drawCircle() {
+    
+}
+
+function drawLine() {
+    
+}
+
+function drawTextAtPoint() {
+    
+}
+
+function getMousePosition() {
+    
+}
+
+function getAngleUsingXAndY() {
+    
+}
+
+function radiansToDegrees() {
+    
+}
+
+function degreesToRadians() {
+    
+}
+
+function drawTriangle() {
+    
+}
+
+function getLineLength() {
+    
+}
+
