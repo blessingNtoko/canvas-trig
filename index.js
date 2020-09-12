7acf100a6b4f0e33347ada1d1a444cb517afafe9
Blessing <bntoko@gmail.com>
2020-09-12 16:50:10 +0200

    added drawRectangle() | added drawCanvas() | added setupCanvas()

--- index.js
+++ index.js
@@ -14,19 +14,27 @@ let mousePos = new MousePosition(0, 0);
 document.addEventListener("DomContentLoaded", setupCanvas);
 
 function setupCanvas() {
-    
+    canvas = document.getElementById("myCanvas");
+    context = canvas.getContext("2d");
+    drawCanvas();
+    canvas.addEventListener("mousemove", reDrawCanvas);
 }
 
 function drawCanvas() {
-    
+    drawRectangle("#839192", 5, 0, 0, 600, 600);
+    drawCircle();
+    drawLine();
+    drawLine();
 }
 
 function reDrawCanvas() {
     
 }
 
-function drawRectangle() {
-    
+function drawRectangle(strokeColor, lineWidth, startX, startY, endX, endY) {
+    context.strokeStyle = strokeColor;
+    context.lineWidth = lineWidth;
+    context.strokeRect(startX, startY, endX, endY);
 }
 
 function drawCircle() {
